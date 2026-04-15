e4cc5b8562d976efdc456ced8939888e77ebd8b5
Sumanth <usumanth@deloitte.com>
2026-04-15 14:04:28 +0530

    added navigation

--- container/src/App.js
+++ container/src/App.js
@@ -1,6 +1,8 @@
-import React, { lazy, Suspense, useState } from "react";
+import React, { lazy, Suspense, useEffect, useState } from "react";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Router, Redirect, Route, Switch } from "react-router-dom";
+
+import { createBrowserHistory } from "history";
 
 import Header from "./components/Header";
 
@@ -8,11 +10,19 @@ const MarketingApp = lazy(() => import("./components/MarketingApp"));
 const AuthApp = lazy(() => import("./components/AuthApp"));
 const DashboardApp = lazy(() => import("./components/DashboardApp"));
 
+const history = createBrowserHistory();
+
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    if (isSignedIn) {
+      history.push("/dashboard");
+    }
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <div>
         <Header
           isSignedIn={isSignedIn}
@@ -23,11 +33,14 @@ export default () => {
             <Route path="/auth">
               <AuthApp onSignIn={() => setIsSignedIn(true)} />
             </Route>
-            <Route path="/dashboard" component={DashboardApp} />
+            <Route path="/dashboard">
+              {!isSignedIn && <Redirect to="/" />}
+              <DashboardApp />
+            </Route>
             <Route path="/" component={MarketingApp} />
           </Switch>
         </Suspense>
       </div>
-    </BrowserRouter>
+    </Router>
   );
 };
